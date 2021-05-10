83430d14c872de87f349efc1ef1f855db8fdd2f2
Diego <diegoti231@gmail.com>
2021-05-10 15:53:13 -0300

    Implementação do get e post de temas

--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 import {environment} from'./../../environments/environment.prod';
 
 @Component({
@@ -9,13 +11,36 @@ import {environment} from'./../../environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-  private router: Router
+  private router: Router,
+  private temaService: TemaService
   ){}
+
   ngOnInit()  {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
+
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
